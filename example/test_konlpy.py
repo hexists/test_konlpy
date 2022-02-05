b71935f2e633ee68e7d325e06526e811650ea785
hexists <hexists@daum.net>
2022-02-06 08:40:30 +0900

    update, example code

--- example/test_konlpy.py
+++ example/test_konlpy.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 
-from konlpy.tag import Hannanum
-hannanum = Hannanum()
+from konlpy.tag import Hannanum, Kkma, Komoran, Mecab, Okt
 
+hannanum = Hannanum()
 print('[Hannanum]')
 print(hannanum.analyze('롯데마트의 흑마늘 양념 치킨이 논란이 되고 있다.'))
 
-from konlpy.tag import Kkma
 kkma = Kkma()
 print('[Kkma]')
 print(kkma.morphs('공부를 하면할수록 모르는게 많다는 것을 알게 됩니다.'))
+
+komoran = Komoran()
+print('[Komoran]')
+print(komoran.morphs(u'우왕 코모란도 오픈소스가 되었어요'))
+
+mecab = Mecab()
+print('[Mecab]')
+print(mecab.morphs(u'영등포구청역에 있는 맛집 좀 알려주세요.'))
+
+okt = Okt()
+print('[Okt]')
+print(okt.morphs(u'단독입찰보다 복수입찰의 경우'))
